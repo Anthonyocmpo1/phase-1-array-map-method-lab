87565a28f986c5b64d0bc337d26feaaa227d37cd
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-10-08 09:16:15 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +29,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    const splittedArr = tutorial.split(" ");
+    const capitalized = splittedArr.map(
+      (str) => str.charAt(0).toUpperCase() + str.slice(1)
+    );
+    return capitalized.join(" ");
+  });
+};
+
+console.log(titleCased());
